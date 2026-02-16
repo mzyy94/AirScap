30f158bda831cc5d0888a279aadc3002e33e0d4f
Yuki MIZUNO <mizuyuu0904@gmail.com>
2026-02-16 21:22:32 +0900

    Add main eSCL bridge server with mDNS advertisement and graceful shutdown

--- cmd/airscap/main.go
+++ cmd/airscap/main.go
@@ -1,7 +1,158 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/OpenPrinting/go-mfp/proto/escl"
+	"github.com/grandcat/zeroconf"
+
+	"github.com/mzyy94/airscap/internal/scanner"
+	"github.com/mzyy94/airscap/internal/vens"
+)
 
 func main() {
-	fmt.Println("airscap: ScanSnap eSCL bridge server")
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelInfo})))
+
+	// Parse configuration from environment variables
+	scannerIP := os.Getenv("AIRSCAP_SCANNER_IP")
+	password := os.Getenv("AIRSCAP_PASSWORD")
+	passwordFile := os.Getenv("AIRSCAP_PASSWORD_FILE")
+	listenPort := envInt("AIRSCAP_LISTEN_PORT", 8080)
+	deviceName := envStr("AIRSCAP_DEVICE_NAME", "ScanSnap iX500")
+
+	// Resolve password
+	if password == "" && passwordFile != "" {
+		data, err := os.ReadFile(passwordFile)
+		if err != nil {
+			slog.Error("failed to read password file", "path", passwordFile, "err", err)
+			os.Exit(1)
+		}
+		password = strings.TrimSpace(string(data))
+	}
+	if password == "" {
+		slog.Error("AIRSCAP_PASSWORD or AIRSCAP_PASSWORD_FILE is required")
+		os.Exit(1)
+	}
+
+	// Compute identity from password
+	identity, err := vens.ComputeIdentity(password)
+	if err != nil {
+		slog.Error("failed to compute identity", "err", err)
+		os.Exit(1)
+	}
+	slog.Info("identity computed", "identity", identity)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	// Discover scanner if IP not specified
+	if scannerIP == "" {
+		slog.Info("discovering scanner...")
+		info, err := vens.FindScanner(ctx, vens.DiscoveryOptions{Timeout: 30 * time.Second})
+		if err != nil {
+			slog.Error("scanner discovery failed", "err", err)
+			os.Exit(1)
+		}
+		scannerIP = info.DeviceIP
+		slog.Info("scanner found", "ip", scannerIP, "name", info.Name, "serial", info.Serial)
+	}
+
+	// Create and connect scanner
+	sc := scanner.New(scannerIP, vens.DefaultDataPort, vens.DefaultControlPort, identity)
+	if err := sc.Connect(ctx); err != nil {
+		slog.Error("scanner connection failed", "err", err)
+		os.Exit(1)
+	}
+	defer sc.Disconnect()
+
+	// Create eSCL adapter
+	adapter := scanner.NewESCLAdapter(sc)
+
+	// Create eSCL HTTP server
+	esclServer := escl.NewAbstractServer(escl.AbstractServerOptions{
+		Scanner:  adapter,
+		BasePath: "/eSCL",
+	})
+
+	mux := http.NewServeMux()
+	mux.Handle("/eSCL/", esclServer)
+
+	addr := fmt.Sprintf(":%d", listenPort)
+	httpServer := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	// Start mDNS advertisement
+	mdnsServer, err := zeroconf.Register(
+		deviceName,
+		"_uscan._tcp",
+		"local.",
+		listenPort,
+		[]string{
+			"txtvers=1",
+			"ty=" + deviceName,
+			"pdl=application/pdf,image/jpeg",
+			"cs=color,grayscale,binary",
+			"is=adf",
+			"duplex=T",
+			"rs=eSCL",
+		},
+		nil,
+	)
+	if err != nil {
+		slog.Error("mDNS registration failed", "err", err)
+		os.Exit(1)
+	}
+	defer mdnsServer.Shutdown()
+	slog.Info("mDNS registered", "name", deviceName, "service", "_uscan._tcp")
+
+	// Start HTTP server
+	go func() {
+		localIP := vens.GetLocalIP()
+		slog.Info("eSCL server starting", "addr", addr, "url", fmt.Sprintf("http://%s/eSCL", net.JoinHostPort(localIP, strconv.Itoa(listenPort))))
+		if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+			slog.Error("HTTP server error", "err", err)
+			cancel()
+		}
+	}()
+
+	// Wait for shutdown signal
+	<-ctx.Done()
+	slog.Info("shutting down...")
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
+		slog.Error("HTTP shutdown error", "err", err)
+	}
+
+	slog.Info("shutdown complete")
+}
+
+func envStr(key, fallback string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return fallback
+}
+
+func envInt(key string, fallback int) int {
+	if v := os.Getenv(key); v != "" {
+		if n, err := strconv.Atoi(v); err == nil {
+			return n
+		}
+	}
+	return fallback
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,18 @@ module github.com/mzyy94/airscap
 
 go 1.25.0
 
-require github.com/OpenPrinting/go-mfp v0.0.0-20260213072225-403537e236b1
+require (
+	github.com/OpenPrinting/go-mfp v0.0.0-20260213072225-403537e236b1
+	github.com/grandcat/zeroconf v1.0.0
+)
 
 require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/miekg/dns v1.1.27 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/image v0.24.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/term v0.29.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
